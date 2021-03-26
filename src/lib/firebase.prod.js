7364326921ec8c3351e1c7096c72e1c288e5fa99
Junaid-sadiq <69793438+Junaid-sadiq@users.noreply.github.com>
2021-03-26 23:39:53 +0500

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -3,15 +3,9 @@ import 'firebase/firestore';
 import 'firebase/auth';
 //import { seedDatabase } from '../seed';
 
+
 const config = {
-        apiKey: "AIzaSyCdGmy2ZfF9BPS67S6uBhOHAV4HguGYrww",
-        authDomain: "netflix-5b1d5.firebaseapp.com",
-        databaseURL: "https://netflix-5b1d5.firebaseio.com",
-        projectId: "netflix-5b1d5",
-        storageBucket: "netflix-5b1d5.appspot.com",
-        messagingSenderId: "96468943773",
-        appId: "1:96468943773:web:77c2e3ade6d5bcf096e574",
-        measurementId: "G-3J5RC3GDDF"
+               //paste your firebase app config here
       };
 
 
